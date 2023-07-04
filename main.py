51b352d056ffe9313f412248252b5d2ebee91455
Christian Radden <102736612+chradden@users.noreply.github.com>
2023-07-04 15:56:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,120 +1,113 @@
+import os
+import whisper
 import streamlit as st
-from transcriber import Transcription
-import docx
-from datetime import datetime
-import pathlib
-import io
-import matplotlib.colors as mcolors
+from pydub import AudioSegment
 
-# app wide config
 st.set_page_config(
-    page_title="Whisper",
+    page_title="Whisper based ASR",
+    page_icon="musical_note",
     layout="wide",
-    page_icon="💬"
+    initial_sidebar_state="auto",
 )
 
-# load stylesheet
-with open('style.css') as f:
-    st.markdown('<style>{}</style>'.format(f.read()),
-                unsafe_allow_html=True)
+audio_tags = {'comments': 'Converted using pydub!'}
 
-# app sidebar for uplading audio files
-with st.sidebar.form("input_form"):
-    input_files = st.file_uploader(
-        "Files", type=["mp4", "m4a", "mp3", "wav"], accept_multiple_files=True)
+upload_path = "uploads/"
+download_path = "downloads/"
+transcript_path = "transcripts/"
 
-    whisper_model = st.selectbox("Whisper model", options=[
-        "tiny", "base", "small", "medium", "large"], index=4)
+@st.cache(persist=True,allow_output_mutation=False,show_spinner=True,suppress_st_warning=True)
+def to_mp3(audio_file, output_audio_file, upload_path, download_path):
+    ## Converting Different Audio Formats To MP3 ##
+    if audio_file.name.split('.')[-1].lower()=="wav":
+        audio_data = AudioSegment.from_wav(os.path.join(upload_path,audio_file.name))
+        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
 
-    pauses = st.checkbox("Pausen transkribieren", value=False)
+    elif audio_file.name.split('.')[-1].lower()=="mp3":
+        audio_data = AudioSegment.from_mp3(os.path.join(upload_path,audio_file.name))
+        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
 
-    translation = st.checkbox("Englische Übersetzung", value=False)
+    elif audio_file.name.split('.')[-1].lower()=="ogg":
+        audio_data = AudioSegment.from_ogg(os.path.join(upload_path,audio_file.name))
+        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
 
-    transcribe = st.form_submit_button(label="Start")
+    elif audio_file.name.split('.')[-1].lower()=="wma":
+        audio_data = AudioSegment.from_file(os.path.join(upload_path,audio_file.name),"wma")
+        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
 
-if transcribe:
-    if input_files:
-        st.session_state.transcription = Transcription(
-            input_files)
-        st.session_state.transcription.transcribe(
-            whisper_model, translation
-        )
-    else:
-        st.error("Bitte wählen Sie eine Datei")
+    elif audio_file.name.split('.')[-1].lower()=="aac":
+        audio_data = AudioSegment.from_file(os.path.join(upload_path,audio_file.name),"aac")
+        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
 
-# if there is a transcription, render it. If not, display instructions
-if "transcription" in st.session_state:
+    elif audio_file.name.split('.')[-1].lower()=="flac":
+        audio_data = AudioSegment.from_file(os.path.join(upload_path,audio_file.name),"flac")
+        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
 
-    for i, output in enumerate(st.session_state.transcription.output):
-        doc = docx.Document()
-        avg_confidence_score = 0
-        amount_words = 0
-        save_dir = str(pathlib.Path(__file__).parent.absolute()
-                       ) + "/transcripts/"
-        st.markdown(
-            f"#### Transkription von {output['name']}")
-        for idx, segment in enumerate(output['segments']):
-            for w in output['segments'][idx]['words']:
-                amount_words += 1
-                avg_confidence_score += w['probability']
-        st.markdown(
-            f"_(whisper model:_`{whisper_model}` -  _language:_ `{output['language']}` -  _⌀ confidence score:_ `{round(avg_confidence_score / amount_words,3)}`)")
-        prev_word_end = -1
-        text = ""
-        html_text = ""
-        # Define the color map
-        colors = [(0.6, 0, 0), (1, 0.7, 0), (0, 0.6, 0)]
-        cmap = mcolors.LinearSegmentedColormap.from_list('my_colormap', colors)
+    elif audio_file.name.split('.')[-1].lower()=="flv":
+        audio_data = AudioSegment.from_flv(os.path.join(upload_path,audio_file.name))
+        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
 
-        with st.expander("Transkript"):
-            color_coding = st.checkbox(
-                "Farbkodierung", value=False, key={i}, help='Farbkodierung eines Wortes auf der Grundlage der Wahrscheinlichkeit, dass es richtig erkannt wurde. Die Farbskala reicht von grün (hoch) bis rot (niedrig).')
-            for idx, segment in enumerate(output['segments']):
-                for w in output['segments'][idx]['words']:
-                    # check for pauses in speech longer than 3s
-                    if pauses and prev_word_end != -1 and w['start'] - prev_word_end >= 3:
-                        pause = w['start'] - prev_word_end
-                        pause_int = int(pause)
-                        html_text += f'{"."*pause_int}{{{pause_int}sek}}'
-                        text += f'{"."*pause_int}{{{pause_int}sek}}'
-                    prev_word_end = w['end']
-                    if (color_coding):
-                        rgba_color = cmap(w['probability'])
-                        rgb_color = tuple(round(x * 255)
-                                          for x in rgba_color[:3])
-                    else:
-                        rgb_color = (0, 0, 0)
-                    html_text += f"<span style='color:rgb{rgb_color}'>{w['word']}</span>"
-                    text += w['word']
-                    # insert line break if there is a punctuation mark
-                    if any(c in w['word'] for c in "!?.") and not any(c.isdigit() for c in w['word']):
-                        html_text += "<br><br>"
-                        text += '\n\n'
-            st.markdown(html_text, unsafe_allow_html=True)
-            doc.add_paragraph(text)
+    elif audio_file.name.split('.')[-1].lower()=="mp4":
+        audio_data = AudioSegment.from_file(os.path.join(upload_path,audio_file.name),"mp4")
+        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
+    return output_audio_file
 
-        if (translation):
-            with st.expander("Englische Übersetzung"):
-                st.markdown(output["translation"], unsafe_allow_html=True)
+@st.cache(persist=True,allow_output_mutation=False,show_spinner=True,suppress_st_warning=True)
+def process_audio(filename, model_type):
+    model = whisper.load_model(model_type)
+    result = model.transcribe(filename)
+    return result["text"]
 
-        # save transcript as docx. in local folder
-        file_name = output['name'] + "-" + whisper_model + \
-            "-" + datetime.today().strftime('%d-%m-%y') + ".docx"
-        doc.save(save_dir + file_name)
+@st.cache(persist=True,allow_output_mutation=False,show_spinner=True,suppress_st_warning=True)
+def save_transcript(transcript_data, txt_file):
+    with open(os.path.join(transcript_path, txt_file),"w") as f:
+        f.write(transcript_data)
 
-        bio = io.BytesIO()
-        doc.save(bio)
-        st.download_button(
-            label="Download Transkript",
-            data=bio.getvalue(),
-            file_name=file_name,
-            mime="docx"
-        )
+st.title("🗣 Automatic Speech Recognition using whisper by OpenAI ✨")
+st.info('✨ Supports all popular audio formats - WAV, MP3, MP4, OGG, WMA, AAC, FLAC, FLV 😉')
+uploaded_file = st.file_uploader("Upload audio file", type=["wav","mp3","ogg","wma","aac","flac","mp4","flv"])
+
+audio_file = None
+
+if uploaded_file is not None:
+    audio_bytes = uploaded_file.read()
+    with open(os.path.join(upload_path,uploaded_file.name),"wb") as f:
+        f.write((uploaded_file).getbuffer())
+    with st.spinner(f"Processing Audio ... 💫"):
+        output_audio_file = uploaded_file.name.split('.')[0] + '.mp3'
+        output_audio_file = to_mp3(uploaded_file, output_audio_file, upload_path, download_path)
+        audio_file = open(os.path.join(download_path,output_audio_file), 'rb')
+        audio_bytes = audio_file.read()
+    print("Opening ",audio_file)
+    st.markdown("---")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("Feel free to play your uploaded audio file 🎼")
+        st.audio(audio_bytes)
+    with col2:
+        whisper_model_type = st.radio("Please choose your model type", ('Tiny', 'Base', 'Small', 'Medium', 'Large'))
+
+    if st.button("Generate Transcript"):
+        with st.spinner(f"Generating Transcript... 💫"):
+            transcript = process_audio(str(os.path.abspath(os.path.join(download_path,output_audio_file))), whisper_model_type.lower())
+
+            output_txt_file = str(output_audio_file.split('.')[0]+".txt")
+
+            save_transcript(transcript, output_txt_file)
+            output_file = open(os.path.join(transcript_path,output_txt_file),"r")
+            output_file_data = output_file.read()
+
+        if st.download_button(
+                             label="Download Transcript 📝",
+                             data=output_file_data,
+                             file_name=output_txt_file,
+                             mime='text/plain'
+                         ):
+            st.balloons()
+            st.success('✅ Download Successful !!')
 
 else:
-    # show instruction page
-    st.markdown("<h1>WHISPER - AUTOMATISCHE TRANSKRIPTION </h1> <p> Dieses Projekt wurde im Rahmen der Masterarbeit von <a href='mailto:johanna.jaeger89@icloud.com'> Johanna Jäger<a/> " +
-                "unter der Verwendung von <a href='https://openai.com/blog/whisper'> OpenAI Whisper</a> durchgeführt.</p> <h2 class='highlight'>DATENSCHUTZ: </h2> <p>Das Programm wird lokal ausgeführt. " +
-                "Die Transkripte werden in ein lokales Verzeichnis dieses PCs gespeichert. </p><h2 class='highlight'>VERWENDUNG: </h2> <ol><li> Wählen Sie die Dateien aus, die Sie transkribieren lassen möchten (mehrere Dateien möglich)</li>" +
-                "<li>  Wählen Sie ein Modell (<i>large</i> für das beste Resultat) und andere Parameter aus und klicken Sie auf 'Start'</li> <li>  Sehen Sie sich die entstandenen Transkripte im <i>transcripts</i>-Ordner dieses Verzeichnisses an </li></ol>",
-                unsafe_allow_html=True)
+    st.warning('⚠ Please upload your audio file 😯')
+
+st.markdown("<br><hr><center>Made with ❤️ by <a href='mailto:ralhanprateek@gmail.com?subject=ASR Whisper WebApp!&body=Please specify the issue you are facing with the app.'><strong>Prateek Ralhan</strong></a> with the help of [whisper](https://github.com/openai/whisper) built by [OpenAI](https://github.com/openai) ✨</center><hr>", unsafe_allow_html=True)
+
